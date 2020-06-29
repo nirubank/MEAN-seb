73b54d5b7090c133ef64fbe0c38376bcd6675d49
nirubank <39207008+nirubank@users.noreply.github.com>
2020-06-29 13:32:14 +0000

    Update db.js

--- MEAN-FINAL/NodeJS/db.js
+++ MEAN-FINAL/NodeJS/db.js
@@ -3,8 +3,18 @@ const mongoose = require('mongoose');
 //const dbHost = 'mongodb://database/CrudDB';
 //'mongodb://localhost:27017/CrudDB'
 
+var connection_string = '127.0.0.1:27017/YOUR_APP_NAME';
+  // if OPENSHIFT env variables are present, use the available connection info:
+  if(process.env.OPENSHIFT_MONGODB_DB_PASSWORD){
+    connection_string = process.env.OPENSHIFT_MONGODB_DB_USERNAME + ":" +
+    process.env.OPENSHIFT_MONGODB_DB_PASSWORD + "@" +
+    process.env.OPENSHIFT_MONGODB_DB_HOST + ':' +
+    process.env.OPENSHIFT_MONGODB_DB_PORT + '/' +
+    process.env.OPENSHIFT_APP_NAME;
+  };
+  //Connect to mongodb using mongoose library
 
-mongoose.connect('mongodb://database/CrudDB', (err) => {
+mongoose.connect(connection_string, (err) => {
     if (!err)
         console.log('MongoDB connection succeeded.');
     else
