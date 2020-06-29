0889a7001a1f11d476bc9e5f6cd1308f924a3c23
nirubank <39207008+nirubank@users.noreply.github.com>
2020-06-29 16:22:45 +0000

    Update environment.prod.ts

--- MEAN-FINAL/AngularApp/src/environments/environment.prod.ts
+++ MEAN-FINAL/AngularApp/src/environments/environment.prod.ts
@@ -1,3 +1,5 @@
 export const environment = {
-  production: true
+  production: true,
+  debugMode: true,
+  backendHostname: 'mean-seb-git-nirubanmeanstacktest3.apps.cluster3.openshift3.iets.com'
 };
