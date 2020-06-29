b5ca0048c76b4498dcba906c59e13dcc871d8204
nirubank <39207008+nirubank@users.noreply.github.com>
2020-06-29 14:07:29 +0000

    Update index.js

--- MEAN-FINAL/NodeJS/index.js
+++ MEAN-FINAL/NodeJS/index.js
@@ -9,7 +9,9 @@ var app = express();
 app.use(bodyParser.json());
 app.use(cors({ origin: 'http://localhost:4200' }));
 
-app.listen(3000, () => console.log('Server started at port : 3000'));
+var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080,
+    ip = process.env.IP || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0';
 
+app.listen(port, ip, () => console.log('Server started at port : '+port+" IP"+ port));
 
 app.use('/employees', employeeController);
