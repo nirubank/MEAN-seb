71757e8c31013846d18ca5f0f6fdd0e583201fc2
nirubank <39207008+nirubank@users.noreply.github.com>
2020-06-29 16:22:21 +0000

    Update environment.ts

--- MEAN-FINAL/AngularApp/src/environments/environment.ts
+++ MEAN-FINAL/AngularApp/src/environments/environment.ts
@@ -4,5 +4,7 @@
 // The list of which env maps to which file can be found in `.angular-cli.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  debugMode: true,
+  backendHostname: 'mean-seb-git-nirubanmeanstacktest3.apps.cluster3.openshift3.iets.com'
 };
