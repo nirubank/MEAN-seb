ac8d72398339b9a2678bfa7ed308721fcc646468
nirubank <39207008+nirubank@users.noreply.github.com>
2020-06-29 15:35:40 +0000

    Update index.js

--- MEAN-FINAL/NodeJS/index.js
+++ MEAN-FINAL/NodeJS/index.js
@@ -8,6 +8,8 @@ var employeeController = require('./controllers/employeeController.js');
 var app = express();
 app.use(bodyParser.json());
 
+// load the routes
+require('./app/routes')(app);
 
 var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080,
     ip = process.env.IP || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0';
@@ -15,3 +17,5 @@ var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080,
 app.listen(port, ip, () => console.log('Server started at port : '+port+" IP"+ ip));
 
 app.use('/employees', employeeController);
+
+module.exports = app;
